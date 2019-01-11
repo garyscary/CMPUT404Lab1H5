bc2e113ace681674f19b1c8275fdb59c892de795
Gary Dhillon <gary.dhil@gmail.com>
2019-01-10 17:56:22 -0700

    Modified script to fetch its own source code and output itself.

--- requests_test.py
+++ requests_test.py
@@ -4,7 +4,7 @@ import requests
 
 print(requests.__version__)
 
-response = requests.get("http://www.google.com")
+response = requests.get("https://raw.githubusercontent.com/garyscary/CMPUT404Lab1H5/master/requests_test.py")
 # print(dir(response))
 print(response.text)
-print(response.status_code)
+# print(response.status_code)
